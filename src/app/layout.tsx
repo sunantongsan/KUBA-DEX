9d5da341a4f81e2541d62671e6596c5b8e41b1a0
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2025-11-19 09:35:59 +0700

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
+export const metadata: Metadata = {
+  title: "KUBA SWAP",
+  description: "DEX on TON • Powered by $KUBA",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="th">
+      <body>
+        <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/sunantongsan/KUBA-DEX/main/public/tonconnect-manifest.json">
+          {children}
+        </TonConnectUIProvider>
+      </body>
+    </html>
+  );
+}
