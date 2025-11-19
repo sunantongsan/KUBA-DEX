762f3b864017ceb3417002909770d49999a444f3
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2025-11-19 10:01:59 +0700

    Create not-found.jsx

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center text-center p-8">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <p className="text-2xl mb-8">Page Not Found</p>
+      <a href="/" className="text-yellow-400 underline text-xl">กลับหน้าหลัก</a>
+    </main>
+  );
+}
