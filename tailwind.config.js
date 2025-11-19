0e563dd92934e5e93c1cac9b0bdcc2780137713a
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2025-11-19 09:14:38 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#FFD700",
+        darkgold: "#B8860B",
+      },
+    },
+  },
+  plugins: [],
+}
